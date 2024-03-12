4c80f9fbc267d39c890025c526e1131867482b32
DERECK_ADRIAN_SANDOVAL <a.21308051280412@cbtis128.edu.mx>
2024-03-12 17:46:20 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MiApp0412 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: "/",
       routes: {
         "/": (context) => Pantallainicial_0412(),
